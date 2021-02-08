8f9b54acd7a411a3029ab5cf4eddd7d7257a065a
Ashmeet Singh <ashmeetsingh2010@gmail.com>
2021-02-08 14:14:05 -0500

    f

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,11 +2,15 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var height = $(window).scrollTop();
         console.log(height)
-        if (height > 400) {
+        if (height > 300) {
             $(".sec-img").addClass("dekho")
             $(".sec-img").removeClass("sec");
             $(".sec-cont").addClass("sec-count-appear")
             $(".sec-cont").removeClass("sec-c")
+        }
+        if (height > 900) {
+            $(".leftimg").addClass("dekho");
+            $(".leftimg").removeClass("hide");
         }
     });
 });
