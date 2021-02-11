00f7f1fd436589bf2fc34c48d0378bf28d31f959
ashmeet singh <ashmeet.singh2010@gmail.com>
2021-02-10 21:37:36 -0500

    pa

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var height = $(window).scrollTop();
         console.log(height)
-        if (height > 300) {
+        if (height > 200) {
             $(".sec-img").addClass("dekho")
             $(".sec-img").removeClass("sec");
             $(".sec-cont").addClass("sec-count-appear")
             $(".sec-cont").removeClass("sec-c")
         }
-        if (height > 900) {
+        if (height > 800) {
             $(".leftimg").addClass("dekho");
             $(".leftimg").removeClass("hide");
         }
