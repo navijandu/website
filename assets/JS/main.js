b5d943d454608d363a78b1d3202c13c425b11c7c
ashmeet singh <ashmeet.singh2010@gmail.com>
2021-02-11 14:05:02 -0500

    f

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var height = $(window).scrollTop();
         console.log(height)
+
+        if (height > 400) {
+            $(".bl").addClass("bl-visible")
+            $(".bl ul li").addClass("bl-color")
+        } else {
+            $("bl").removeClass("bl-visible")
+        }
         if (height > 200) {
             $(".sec-img").addClass("dekho")
             $(".sec-img").removeClass("sec");
@@ -11,6 +18,14 @@ $(document).ready(function () {
         if (height > 800) {
             $(".leftimg").addClass("dekho");
             $(".leftimg").removeClass("hide");
+            $(".rightimg").addClass("fromright");
+            $(".rightimg").removeClass("hideright");
+        }
+        if (height > 1300) {
+            $(".hide2").addClass("dekho");
+            $(".hide2").removeClass("leftimg2");
+            $(".rightimg2").addClass("fromright")
+            $(".rightimg2").removeClass("hideright2")
         }
     });
 });
